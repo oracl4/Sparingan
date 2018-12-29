75e83f612a931c9fcb95d6a8c7bbcd6fa0eba8ac
gilangyudharaka <44997954+gilangyudharaka@users.noreply.github.com>
2018-12-29 23:14:27 +0700

    Add files via upload

---
+++ app/src/main/java/com/sparingan/saveUid.java
@@ -0,0 +1,33 @@
+package com.sparingan;
+
+
+/**
+ * Referensi
+ * www.androidhive.info
+ */
+
+public class saveUid {
+
+
+
+    public String suid;
+
+    public saveUid(){
+
+    }
+    public saveUid(String suid) {
+
+        this.suid=suid;
+    }
+
+    public String getSuid() {
+        return suid;
+    }
+
+    public void setSuid(String suid) {
+        this.suid = suid;
+    }
+
+}
+
+
